df8a90673a2ff59284159268dafdf09fe6083d36
anya-kashchenko <143807908+anna-klbn@users.noreply.github.com>
2023-12-05 21:48:32 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import './css/styles.css';
+// import './css/styles.css';
 
-import { fetchBreeds } from "./cat-api";
+
 
 const select = document.querySelector('.breed-select');
 const error = document.querySelector('.error');
@@ -8,53 +8,48 @@ const loader = document.querySelector('.loader');
 const info = document.querySelector('.cat-info');
 
 
-loader.classList.replace('loader', 'is-hidden');
-error.classList.add('is-hidden');
-info.classList.add('is-hidden');
+
+const url = "https://api.thecatapi.com/v1";
+const api_key = "live_nBcdwsZCOeUWf4vXmur3MVuwar83P0nhjuBSI8o7dFRduj5FItkahjYz2ItOkE5m";
 
 
-select.addEventListener('change', onSelectBreed)
-
-let arrBreedsId = [];
-fetchBreeds()
-.then(data => {
-    data.forEach(element => {
-        arrBreedsId.push({text: element.name, value: element.id});
-    });
-  new SlimSelect({
-        select: select,
-        data: arrBreedsId
-    });
-    })
-.catch(onFetchError);
-
-
-function onSelectBreed (event) {
-    event.preventDefault();
-
-    loader.classList.replace('is-hidden', 'loader');
-    select.classList.add('is-hidden');
-    info.classList.add('is-hidden');
-
-    const breedId = event.currentTarget.value;
-    fetchCatByBreed(breedId)
-        .then(data => {
-            loader.classList.replace('loader', 'is-hidden');
-            select.classList.remove('is-hidden');
-            const { url, breeds } = data[0];
-
-            info.innerHTML = `<div class="box">
-            <img src="${url}" alt="${breeds[0].name}" width="500"/>
-            <h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><h3>Temperament:</h3> ${breeds[0].temperament}</p></div>`
-            info.classList.remove('is-hidden');
-        })
-        .catch(onFetchError)
+// Коллекция пород
+function fetchBreeds() {
+    return fetch(`${url}/breeds?api_key=${api_key}`).then(response => response.json());
+       
 };
 
 
-function onFetchError() {
-    select.classList.remove('is-hidden');
-    loader.classList.replace('loader', 'is-hidden');
+// Информация о коте
+function fetchCatByBreed(breedId) {
+    return fetch(`${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`).then(response => response.json());
+       
+}
 
+let arrBreedsId = [];
+
+fetchBreeds()
+.then(data => {
+    data.forEach(element => {
+        arrBreedsId.push({text:element.name, value:element.id});
+    });
+ })
+    .catch(onFetchError);
+
+select.addEventListener('change', onSelectBreed);
+
+function onSelectBreed(event) {
+    const breedId = event.currentTarget.value;
+    fetchCatByBreed(breedId)
+        .then(data => {
+      const { url, breeds } = data[0];
+        
+        divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><h2>Temperament:</h2> ${breeds[0].temperament}</p></div>`
+        divCatInfo.classList.remove('is-hidden');
+    })
+    .catch(onFetchError);
+};
+
+function onFetchError(error) {
     alert('Oops! Something went wrong! Try reloading the page or select another cat breed!')
 }
